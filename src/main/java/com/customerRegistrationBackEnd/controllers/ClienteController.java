d8df2b19590f73136021c141ed676a706816cd40
M239677 <andersonsoares@sisconsultoria.com.br>
2024-03-25 14:48:13 -0300

    Correção da controller

--- src/main/java/com/customerRegistrationBackEnd/controllers/ClienteController.java
+++ src/main/java/com/customerRegistrationBackEnd/controllers/ClienteController.java
@@ -1,9 +1,11 @@
 package com.customerRegistrationBackEnd.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,21 +20,33 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    /**
-     * @return retona Lista de clientes
-     */
-    @GetMapping
-    public List<ClienteEntity> listarClientes() {
-        return clienteService.listarClientes();
+    @Autowired
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
     }
 
     /**
-     * @param cliente metodo para cadastrar Clientes
+     * @param cadastrarCliente metodo para cadastrar Cliente
      * @return
      */
-    @PostMapping
-    public ClienteEntity cadastrarCliente(@RequestBody ClienteEntity cliente) {
-        return clienteService.cadastrarCliente(cliente);
+    @PostMapping("/cadastrar")
+    public ResponseEntity<ClienteEntity> cadastrarCliente(@Validated @RequestBody ClienteEntity clienteDTO) {
+        ClienteEntity clienteEntity = clienteService.cadastrarCliente(clienteDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteEntity);
+    }
+
+    /**
+     * @param buscarCliente metodo para buscar cliente por Id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteEntity> buscarClientePorId(@PathVariable Long id) {
+        ClienteEntity clienteEntity = clienteService.buscarClientePorId(id);
+        if (clienteEntity != null) {
+            return ResponseEntity.ok(clienteEntity);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
